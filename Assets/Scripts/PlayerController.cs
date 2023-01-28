5df2ce580b3862fb70452bb746b2d7a5794f2470
EricReynolds63 <err63@scarletmail.rutgers.edu>
2023-01-27 19:25:09 -0500

    Finished Version
    Added a second area with ramps between, increased player speed to make sure they can clear the ramps and make the game a bit harder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 	public float speed = 0;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
 	
 	private Rigidbody rb;
+	
+	private int count;
 	
 	private float movementX;
 	private float movementY;
@@ -16,6 +21,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winTextObject.SetActive(false);
     }
 	
 	void OnMove(InputValue movementValue)
@@ -24,6 +32,15 @@ public class PlayerController : MonoBehaviour
 		
 		movementX = movementVector.x;
 		movementY = movementVector.y;
+	}
+	
+	void SetCountText()
+	{
+		countText.text = "Score: " + count.ToString();
+		if (count >= 12)
+		{
+			winTextObject.SetActive(true);
+		}
 	}
 	
 	void FixedUpdate()
@@ -37,6 +54,8 @@ public class PlayerController : MonoBehaviour
 	{
 		if (other.gameObject.CompareTag("PickUp")) {
 			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
 		}
 	}
 
